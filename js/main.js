1cf0b3e2f4422f09b20d8ebe91d9001d1acb5026
Dokiz_228@mail.ru <deoptgqqr12>
2016-11-17 15:43:57 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {},
+		Rout: {}
+    };
+
+    window.template = function(id) {
+        return _.template( $('#' + id). html() );
+    };
+
+    App.Router = Backbone.Router.extend({
+		routes: {
+			'' : 'index',
+			'read' : 'read'
+		},
+		index: function(){
+			console.log('Всем привет');
+		},
+	
+read: function(){
+	console.log('роут');
+}
+});
+new App.Router();
+Backbone.history.start();
+
+	})();
